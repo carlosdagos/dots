7fd03f7315f314f2ea5e6a1c815b040346aae379
Carlos D <carlos.dagostino@gmail.com>
2016-08-15 19:50:39 +0200

    fix PATH in fish config

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -5,6 +5,14 @@ source $fisher_home/config.fish
 alias emacs 'emacs -nw'
 alias bubu  'brew update; brew outdated; brew upgrade; brew cleanup'
 
-set PATH $PATH $HOME/Library/Haskell/bin
-set PATH $PATH $HOME/.local/bin
-set PATH $PATH $HOME/.bin/
+if test -d $HOME/Library/Haskell/bin
+    set PATH $PATH $HOME/Library/Haskell/bin
+end
+
+if test -d $HOME/.local/bin
+    set PATH $PATH $HOME/.local/bin
+end
+
+if test -d $HOME/.bin
+    set PATH $PATH $HOME/.bin
+end
