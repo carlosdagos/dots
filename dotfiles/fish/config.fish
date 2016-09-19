7f8d2b656b09c1f561565849c13fcccfae706393
Carlos D'Agostino <carlos.dagostino@gmail.com>
2016-09-19 16:56:48 +0200

    finally got the damn gvm+fish combo working

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -20,5 +20,23 @@ if test -d $HOME/.bin
 end
 
 if test -d $HOME/.gvm
-   gvm use go1.7 --default > /dev/null
+   set go_version "go1.7"
+   set GVM_ROOT "$HOME/.gvm"
+
+   gvm use $go_version --default > /dev/null
+
+   export GVM_ROOT
+   export gvm_go_name
+   export gvm_pkgset_name
+   export GOROOT
+   export GOPATH
+   export GVM_OVERLAY_PREFIX
+   export LD_LIBRARY_PATH
+   export DYLD_LIBRARY_PATH
+   export PKG_CONFIG_PATH
+
+   set PATH $PATH "$GVM_ROOT/pkgsets/$go_version/global/bin"
+   set PATH $PATH "$GVM_ROOT/gos/$go_version/bin"
+   set PATH $PATH "$GVM_OVERLAY_PREFIX/bin"
+   set PATH $PATH "$GVM_ROOT/bin"
 end
