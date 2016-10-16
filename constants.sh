e6e82daf955e92adcb678a3b7da061b66d203759
Carlos D <carlos.dagostino@gmail.com>
2016-10-16 21:44:47 +0200

    use $HOME explicitly

--- constants.sh
+++ constants.sh
@@ -2,12 +2,12 @@
 # Define some constants
 #
 
-SMM_DIR=~/.setup-my-mac
+SMM_DIR=$HOME/.setup-my-mac
 
 SMM_STATUS_SUCCESS="setup_my_mac_status_success"
 SMM_STATUS_ERROR="setup_my_mac_status_error"
 
-SMM_LOGFILE=~/.setup-my-mac/smm.log
+SMM_LOGFILE=$HOME/.setup-my-mac/smm.log
 
 SMM_INSTALL_HOMEBREW='/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"'
 SMM_INSTALL_HOMEBREW_CASK="brew tap caskroom/cask"
