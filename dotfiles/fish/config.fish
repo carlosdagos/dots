fc1f9d90a84730d29a849d85e618b4b5def3fa77
CarlosD <carlos.dagostino@gmail.com>
2019-08-16 14:55:39 +1000

    Small changes to Emacs
    Add more packages

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -1,10 +1,20 @@
+# In osx, prioritize the nix load path
+switch (uname)
+    case Darwin
+        set -gx PATH /run/current-system/sw/bin $PATH
+end
+
 # My aliases
 alias nvim  'nvim -u ~/.nvimrc'
+alias ls    'exa'
 alias l     'ls -la'
 alias la    'ls -la'
 
 set -gx LC_ALL en_US.UTF-8
 set -gx LANG en_US.UTF-8
+
+# Use the `falcon` colors for exa
+set -gx EXA_COLORS 'uu=38;5;249:un=38;5;241:gu=38;5;245:gn=38;5;241:da=38;5;245:sn=38;5;7:sb=38;5;7:ur=38;5;3;1:uw=38;5;5;1:ux=38;5;1;1:ue=38;5;1;1:gr=38;5;249:gw=38;5;249:gx=38;5;249:tr=38;5;249:tw=38;5;249:tx=38;5;249:fi=38;5;248:di=38;5;253:ex=38;5;1:xa=38;5;12:*.png=38;5;4:*.jpg=38;5;4:*.gif=38;5;4'
 
 function ecw --description 'Opens up emacsclient in windowed mode'
     fish -c "emacsclient -c  \\
