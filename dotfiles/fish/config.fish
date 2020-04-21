5d2428a1b465cb42beeaf1cc38f9f62de913c35c
Carlos D'Agostino <m@cdagostino.io>
2020-04-21 21:07:18 +1000

    Use Doom Emacs
    And some minor updates. Notably I finally fixed my termite color scheme
    :D

--- dotfiles/fish/config.fish
+++ dotfiles/fish/config.fish
@@ -17,15 +17,7 @@ set -gx LANG en_US.UTF-8
 set -gx EXA_COLORS 'uu=38;5;249:un=38;5;241:gu=38;5;245:gn=38;5;241:da=38;5;245:sn=38;5;7:sb=38;5;7:ur=38;5;3;1:uw=38;5;5;1:ux=38;5;1;1:ue=38;5;1;1:gr=38;5;249:gw=38;5;249:gx=38;5;249:tr=38;5;249:tw=38;5;249:tx=38;5;249:fi=38;5;248:di=38;5;253:ex=38;5;1:xa=38;5;12:*.png=38;5;4:*.jpg=38;5;4:*.gif=38;5;4'
 
 function ecw --description 'Opens up emacsclient in windowed mode'
-    fish -c "emacsclient -c  \\
-                         -e '(progn
-                               (require \'seq)
-                               (let ((files (seq-filter (lambda (arg) (not (string-prefix-p \"-\" arg)))
-                                                        (split-string \"$argv\" \" \"))))
-                                 (set-frame-font \"Luculent-9\")
-                                 (tool-bar-mode -1)
-                                 (loop for file in files
-                                       do (find-file file))))' &"
+    fish -c "emacsclient -c $argv &"
 end
 
 function ec --description 'Opens up emacsclient in terminal mode'
--- run.sh
+++ run.sh
@@ -66,7 +66,7 @@ function setup_dotfiles {
   soft_link "dotfiles/gitconfig"         "$HOME/.gitconfig"
   soft_link "dotfiles/vimrc"             "$HOME/.vimrc"
   soft_link "dotfiles/nvimrc"            "$HOME/.nvimrc"
-  soft_link "dotfiles/emacs"             "$HOME/.emacs"
+  soft_link "dotfiles/doom.d"            "$HOME/.doom.d"
   soft_link "dotfiles/muttrc"            "$HOME/.muttrc"
   soft_link "dotfiles/mutt"              "$HOME/.mutt"
   soft_link "dotfiles/msmtprc"           "$HOME/.msmtprc"
